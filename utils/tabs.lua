11ec824a9f9ae4f1886d7b3e579914a3d431e9df
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-08-17 09:30:35 +0300

    Indenting and formatting was off

--- utils/tabs.lua
+++ utils/tabs.lua
@@ -3,15 +3,15 @@ local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 local function get_tab_index(window)
-       local tab_id = window:active_tab():tab_id()
-       local tabs = window:mux_window():tabs_with_info()
-       for _, item in ipairs(tabs) do
-              if item.is_active then
-                     return item.index
-              end
-       end
+    local tab_id = window:active_tab():tab_id()
+    local tabs = window:mux_window():tabs_with_info()
+    for _, item in ipairs(tabs) do
+        if item.is_active then
+            return item.index
+        end
+    end
 end
 
 return {
-       get_tab_index = get_tab_index
+    get_tab_index = get_tab_index
 }
--- wezterm.lua
+++ wezterm.lua
@@ -30,68 +30,68 @@ config.send_composed_key_when_left_alt_is_pressed = true
 
 -- Background Image --
 config.background = {
-	{
-		source = { File = wezterm.config_dir .. "/backgrounds/crt-term.jpg" },
-		opacity = 0.90,
-		hsb = {
-			hue = 0.9,
-			saturation = 0.6,
-			brightness = 0.1,
-		},
-	},
+    {
+        source = { File = wezterm.config_dir .. "/backgrounds/crt-term.jpg" },
+        opacity = 0.90,
+        hsb = {
+            hue = 0.9,
+            saturation = 0.6,
+            brightness = 0.1,
+        },
+    },
 }
 
 -- Full screen on startup
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():toggle_fullscreen()
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
 end)
 
 -- mux status
 wezterm.on("update-right-status", function(window, _)
-	local SOLID_LEFT_ARROW = " " .. utf8.char(0xe0b2)
-	local SOLID_RIGHT_ARROW = utf8.char(0xe0b0) .. " "
-	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-	local date = wezterm.strftime("%Y-%m-%d %H:%M")
-	local prefix = ""
+    local SOLID_LEFT_ARROW = " " .. utf8.char(0xe0b2)
+    local SOLID_RIGHT_ARROW = utf8.char(0xe0b0) .. " "
+    local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+    local date = wezterm.strftime("%Y-%m-%d %H:%M")
+    local prefix = ""
 
-	if window:leader_is_active() then
-		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
-		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-	end
+    if window:leader_is_active() then
+        prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+    end
 
+    if tabs.get_tab_index(window) ~= 0 then
+        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+    end -- arrow color based on if tab is first pane
 
-	window:set_right_status(wezterm.format({
-		{ Background = { Color = "#b7bdf8" } },
-		{ Foreground = { Color = "#1e2030" } },
-		{ Text = SOLID_RIGHT_ARROW },
-		{ Text = date },
-	}))
+    window:set_right_status(wezterm.format({
+        { Background = { Color = "#b7bdf8" } },
+        { Foreground = { Color = "#1e2030" } },
+        { Text = SOLID_RIGHT_ARROW },
+        { Text = date },
+    }))
 
-	window:set_left_status(wezterm.format({
-		{ Background = { Color = "#b7bdf8" } },
-		{ Text = prefix },
-		ARROW_FOREGROUND,
-		{ Text = SOLID_LEFT_ARROW },
-	}))
-        if tabs.get_tab_index(window) ~= 0 then
-                ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-        end -- arrow color based on if tab is first pane
+    window:set_left_status(wezterm.format({
+        { Background = { Color = "#b7bdf8" } },
+        { Text = prefix },
+        ARROW_FOREGROUND,
+        { Text = SOLID_LEFT_ARROW },
+    }))
 end)
 
 -- Set the Window title
 wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
-	local zoomed = ""
-	if tab.active_pane.is_zoomed then
-		zoomed = "[Z] "
-	end
+    local zoomed = ""
+    if tab.active_pane.is_zoomed then
+        zoomed = "[Z] "
+    end
 
-	local index = ""
-	if #tabs > 1 then
-		index = string.format("[%d/%d] ", tab.tab_index + 1, #tabs)
-	end
+    local index = ""
+    if #tabs > 1 then
+        index = string.format("[%d/%d] ", tab.tab_index + 1, #tabs)
+    end
 
-	return zoomed .. index .. tab.active_pane.title
+    return zoomed .. index .. tab.active_pane.title
 end)
 
 -- This function returns the suggested title for a tab.
@@ -99,23 +99,23 @@ end)
 -- or `wezterm cli set-tab-title`, but falls back to the
 -- title of the active pane in that tab.
 local function tab_title(tab_info)
-	local title = tab_info.tab_title
-	-- if the tab title is explicitly set, take that
-	if title and #title > 0 then
-		return title
-	end
-	-- Otherwise, use the title from the active pane
-	-- in that tab
-	return tab_info.active_pane.title
+    local title = tab_info.tab_title
+    -- if the tab title is explicitly set, take that
+    if title and #title > 0 then
+        return title
+    end
+    -- Otherwise, use the title from the active pane
+    -- in that tab
+    return tab_info.active_pane.title
 end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, conf, hover, max_width)
-	local title = tab_title(tab)
-	local zoomed = ""
-	if tab.active_pane.is_zoomed then
-		zoomed = utf8.char(0x1F50D) .. " "
-	end
-	return " " .. tab.tab_index + 1 .. ": " .. zoomed .. title .. " "
+    local title = tab_title(tab)
+    local zoomed = ""
+    if tab.active_pane.is_zoomed then
+        zoomed = utf8.char(0x1F50D) .. " "
+    end
+    return " " .. tab.tab_index + 1 .. ": " .. zoomed .. title .. " "
 end)
 
 -- Load the keybindings from keys.lua --
