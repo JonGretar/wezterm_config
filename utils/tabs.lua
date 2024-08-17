ca9e83756469527027fa90fadaebcd3b4b75343e
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-08-17 13:00:23 +0300

    tab utils fix

--- utils/tabs.lua
+++ utils/tabs.lua
@@ -1,17 +1,14 @@
-local wezterm = require("wezterm")
+-- local wezterm = require("wezterm")
 -- local act = wezterm.action
-local mux = wezterm.mux
+local tabs = {}
 
-local function get_tab_index(window)
-    local tab_id = window:active_tab():tab_id()
-    local tabs = window:mux_window():tabs_with_info()
-    for _, item in ipairs(tabs) do
-        if item.is_active then
-            return item.index
-        end
-    end
+function tabs.get_tab_index(window)
+	local tab_list = window:mux_window():tabs_with_info()
+	for _, item in ipairs(tab_list) do
+		if item.is_active then
+			return item.index
+		end
+	end
 end
 
-return {
-    get_tab_index = get_tab_index
-}
+return tabs
