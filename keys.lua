397ffc0a6a1d1ca9ec6e88dad95b834cc53cc267
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-08-23 18:47:53 +0300

    Pane management key tables

--- keys.lua
+++ keys.lua
@@ -3,22 +3,22 @@ local act = wezterm.action
 
 -- Create a function that takes config parameters and returns a config table
 local function make_config(config)
-	config.leader = { key = "b", mods = "CMD", timeout_milliseconds = 2000 }
+	config.leader = { key = "Space", mods = "CMD|SHIFT", timeout_milliseconds = 2000 }
 
 	config.keys = {
-		{ key = "P",          mods = "CMD|SHIFT",  action = wezterm.action.ActivateCommandPalette },
-		{ key = "Enter",      mods = "CMD",        action = act.ActivateCopyMode },
-		{ key = "d",          mods = "CMD|SHIFT",  action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "l",          mods = "CMD|SHIFT",  action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "LeftArrow",  mods = "CMD|SHIFT",  action = act.ActivateTabRelative(-1) },
-		{ key = "RightArrow", mods = "CMD|SHIFT",  action = act.ActivateTabRelative(1) },
-		{ key = "LeftArrow",  mods = "CMD|ALT",    action = act.ActivateTabRelative(-1) },
-		{ key = "RightArrow", mods = "CMD|ALT",    action = act.ActivateTabRelative(1) },
-		{ key = "t",          mods = "CMD|SHIFT",  action = act.ShowTabNavigator },
-		{ key = "H",          mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
-		{ key = "L",          mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
-		{ key = "K",          mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
-		{ key = "J",          mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
+		{ key = "P", mods = "CMD|SHIFT", action = wezterm.action.ActivateCommandPalette },
+		{ key = "Enter", mods = "CMD", action = act.ActivateCopyMode },
+		{ key = "d", mods = "CMD|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "l", mods = "CMD|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "LeftArrow", mods = "CMD|SHIFT", action = act.ActivateTabRelative(-1) },
+		{ key = "RightArrow", mods = "CMD|SHIFT", action = act.ActivateTabRelative(1) },
+		{ key = "LeftArrow", mods = "CMD|ALT", action = act.ActivateTabRelative(-1) },
+		{ key = "RightArrow", mods = "CMD|ALT", action = act.ActivateTabRelative(1) },
+		{ key = "t", mods = "CMD|SHIFT", action = act.ShowTabNavigator },
+		{ key = "H", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
+		{ key = "L", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
+		{ key = "K", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
+		{ key = "J", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
 		{
 			key = "k",
 			mods = "SUPER",
@@ -27,16 +27,58 @@ local function make_config(config)
 				act.SendKey({ key = "L", mods = "CTRL" }),
 			}),
 		},
-		-- Leader Key tmuxing
+
+		-- Acticate Pane Management mode.
 		{
-			key = "|",
+			key = "w",
 			mods = "LEADER",
-			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+			action = act.ActivateKeyTable({
+				name = "pane_management",
+				one_shot = false,
+			}),
 		},
 		{
-			key = "-",
-			mods = "LEADER",
-			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+			key = "W",
+			mods = "LEADER|SHIFT",
+			action = act.ActivateKeyTable({
+				name = "pane_management",
+				timeout_milliseconds = 2000,
+			}),
+		},
+	}
+
+	config.key_tables = {
+		pane_management = {
+			-- Pane Activation
+			{ key = "LeftArrow", action = act.ActivatePaneDirection("Left") },
+			{ key = "h", action = act.ActivatePaneDirection("Left") },
+			{ key = "RightArrow", action = act.ActivatePaneDirection("Right") },
+			{ key = "l", action = act.ActivatePaneDirection("Right") },
+			{ key = "UpArrow", action = act.ActivatePaneDirection("Up") },
+			{ key = "k", action = act.ActivatePaneDirection("Up") },
+			{ key = "DownArrow", action = act.ActivatePaneDirection("Down") },
+			{ key = "j", action = act.ActivatePaneDirection("Down") },
+
+			-- Pane Resizing
+			{ key = "LeftArrow", mods = "SHIFT", action = act.AdjustPaneSize({ "Left", 1 }) },
+			{ key = "h", mods = "SHIFT", action = act.AdjustPaneSize({ "Left", 1 }) },
+			{ key = "RightArrow", mods = "SHIFT", action = act.AdjustPaneSize({ "Right", 1 }) },
+			{ key = "l", mods = "SHIFT", action = act.AdjustPaneSize({ "Right", 1 }) },
+			{ key = "UpArrow", mods = "SHIFT", action = act.AdjustPaneSize({ "Up", 1 }) },
+			{ key = "k", mods = "SHIFT", action = act.AdjustPaneSize({ "Up", 1 }) },
+			{ key = "DownArrow", mods = "SHIFT", action = act.AdjustPaneSize({ "Down", 1 }) },
+			{ key = "j", mods = "SHIFT", action = act.AdjustPaneSize({ "Down", 1 }) },
+
+			-- Split Pane
+			{ key = "s", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+			{ key = "v", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+			-- Zoom pane
+			{ key = "z", action = act.TogglePaneZoomState },
+
+			-- Exit pane management mode
+			{ key = "Escape", action = "PopKeyTable" },
+			{ key = "Enter", action = "PopKeyTable" },
 		},
 	}
 end
