759256e83c025d43b82587d2b58f1119a5d431cf
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-08-27 18:39:52 +0300

    Check for availability

--- utils/helpers.lua
+++ utils/helpers.lua
@@ -1,9 +1,13 @@
--- local wezterm = require("wezterm")
+local wezterm = require("wezterm")
 -- local act = wezterm.action
 local M = {}
 
 function M.get_tab_index(window)
-	local tab_list = window:mux_window():tabs_with_info()
+	local mux_window = window:mux_window()
+	if mux_window == nil then
+		return -1
+	end
+	local tab_list = mux_window:tabs_with_info()
 	for _, item in ipairs(tab_list) do
 		if item.is_active then
 			return item.index
@@ -12,7 +16,11 @@ function M.get_tab_index(window)
 end
 
 function M.get_active_pane_info(pane)
-	local pane_list = pane:tab():panes_with_info()
+	local tab = pane:tab()
+	if tab == nil then
+		return {}
+	end
+	local pane_list = tab:panes_with_info()
 	for _, item in ipairs(pane_list) do
 		if item.is_active then
 			return item
